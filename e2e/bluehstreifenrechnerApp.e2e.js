bc95728cdd9a47600d8167c426b9114365868daa
Alghanem <ghanim.w@gmail.com>
2023-07-30 17:19:03 +0200

    t0.1

--- e2e/bluehstreifenrechnerApp.e2e.js
+++ e2e/bluehstreifenrechnerApp.e2e.js
@@ -1,5 +1,4 @@
-
-const { device, element, by } = require('detox');
+const { device, element, by, expect } = require('detox');
 
 describe('BluehstreifenrechnerApp', () => {
   beforeEach(async () => {
@@ -7,27 +6,20 @@ describe('BluehstreifenrechnerApp', () => {
   });
 
   it('should calculate bee meadow correctly', async () => {
-    // Assuming the app is already launched and the main screen is visible
-
     // Enter Aussaatfläche
-    await element(by.id('aussaatFlächeInput')).typeText('100'); // Replace 'aussaatFlächeInput' with the actual testID of the Aussaatfläche input field
+    await element(by.id('aussaatFlächeInput')).typeText('100');
 
     // Select a Pflanzenart
-    await element(by.id('pflanzenartPicker')).tap(); // Replace 'pflanzenartPicker' with the actual testID of the Pflanzenart picker
-    await element(by.text('Buchweizen')).tap(); // Replace 'Buchweizen' with the desired Pflanzenart
+    await element(by.id('pflanzenartPicker')).tap();
+    await element(by.text('Buchweizen')).tap();
 
     // Enter CO2-Emissionen
-    await element(by.id('co2Input')).typeText('500'); // Replace 'co2Input' with the actual testID of the CO2-Emissionen input field
+    await element(by.id('co2Input')).typeText('500');
 
     // Tap on 'Berechnen'
-    await element(by.text('Berechnen')).tap(); // Replace 'Berechnen' with the text on the button that triggers the Berechnen function
+    await element(by.id('berechnenButton')).tap();
 
     // Assert the results
-    await expect(element(by.id('ergebnisLabel'))).toHaveText('Expected Result'); // Replace 'Expected Result' with the expected result after the calculation
-
-    // Add more assertions for other elements that should be present in the Ergebnis screen
-    // Example:
-    // await expect(element(by.id('gesamtAnzahlBienenProQm'))).toHaveText('Expected Bee Count');
-    // await expect(element(by.id('nahrungFürBienen'))).toHaveText('Expected Bee Food');
+    await expect(element(by.id('ergebnisLabel'))).toHaveText('Expected Result');
   });
 });
